bd1a81a68c44ed4a8b537b8d9e9088c21f965243
Carlos Souza <carlossouza@Confsistemas-MacBook-Air.local>
2023-07-25 10:18:55 -0300

    added columns tables and migrate it

--- database/migrations/2023_07_25_011857_create_cursos_table.php
+++ database/migrations/2023_07_25_011857_create_cursos_table.php
@@ -13,6 +13,23 @@ return new class extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid');
+            $table->integer('id_hotmart');
+            $table->boolean('ad_status');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->string('vsl');
+            $table->string('author');
+            $table->date('date');
+            $table->float('rating');
+            $table->enum('category', ['danzas y bailes', 'música']);
+            $table->enum('format', ['video curso', 'ebook', 'en persona']);
+            $table->enum('subscription', ['pago unico', 'por año', 'por mes']);
+            $table->set('language', ['Español', 'English']);
+            $table->float('comission', 4, 2);
+            $table->decimal('cashback', 5, 2);
+            $table->decimal('price', 6, 2);
             $table->timestamps();
         });
     }
