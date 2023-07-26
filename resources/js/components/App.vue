1e7457a2b221f4b1afd594ad8945c43e66288a0e
Carlos Souza <carlossouza@Confsistemas-MacBook-Air.local>
2023-07-26 12:14:35 -0300

    setup vue js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>{{greetings}} Carlos from Vue JS</h2>
+    <br>
+    <input type="text" v-model="greetings">
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                greetings: ''
+            }
+        }
+    }
+</script>
